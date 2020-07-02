afb477222c3f3e3e718be94985ed9fe883b1c0a6
DawidMalarz <kontakt@dawidmalarz.pl>
2020-07-02 12:25:36 +0200

    Add virtual functions

--- shm/Cargo.hpp
+++ shm/Cargo.hpp
@@ -4,19 +4,21 @@
 
 class Cargo {
 public:
-    Cargo(std::string n = "", size_t a = 0, size_t bp = 0):
-        name_(n), amount_(a), basePrice_(bp) {};
-    
-    std::string getName() const { return name_; }
-    size_t getAmount() const { return amount_; }
-    size_t getBasePrice() const { return basePrice_; }
+    Cargo(std::string n = "", size_t a = 0, size_t bp = 0)
+        : name_(n), amount_(a), basePrice_(bp){};
 
-    Cargo& operator+=(size_t amount);
-    Cargo& operator-=(size_t amount);
-    bool operator==(const Cargo& checkCargo) const;
+    virtual std::string getName() const { return name_; }
+    virtual size_t getAmount() const { return amount_; }
+    virtual size_t getBasePrice() const { return basePrice_; }
+    virtual size_t getPrice() const { return price_; }
+
+    virtual Cargo& operator+=(size_t amount);
+    virtual Cargo& operator-=(size_t amount);
+    virtual bool operator==(const Cargo& checkCargo) const;
 
 protected:
     std::string name_;
     size_t amount_;
     size_t basePrice_;
+    size_t price_;
 };
