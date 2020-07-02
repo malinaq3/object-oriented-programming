10390d3929e8399cbaeddd2256a5c7b8be1650e8
DawidMalarz <kontakt@dawidmalarz.pl>
2020-07-02 12:31:05 +0200

    Add Item class

---
+++ shm/Item.cpp
@@ -0,0 +1,56 @@
+#include "Item.hpp"
+
+size_t Item::getPrice() const {
+    return basePrice_ * static_cast<size_t>(rairty_);
+}
+
+std::string Item::getName() const {
+    return name_;
+}
+
+size_t Item::getAmount() const {
+    return amount_;
+}
+
+size_t Item::getBasePrice() const {
+    return basePrice_;
+}
+Item::Rarity Item::getRarity() const {
+    return rairty_;
+}
+
+Cargo& Item::operator+=(size_t amount) {
+    amount_ += amount;
+    return *this;
+}
+
+Cargo& Item::operator-=(size_t amount) {
+    amount_ -= amount;
+    return *this;
+}
+
+bool Item::operator==(const Cargo& item) const {
+    if (typeid(Item) == typeid(Cargo)) {
+    }
+}
+
+bool Item::operator==(const Cargo& cargo) const {
+    if (typeid(cargo) == typeid(Item)) {
+        const Item* rarity_ptr = static_cast<const Item*>(&cargo);
+        return name_ == rarity_ptr->getName() &&
+               amount_ == rarity_ptr->getAmount() &&
+               basePrice_ == rarity_ptr->getBasePrice() &&
+               rairty_ == rarity_ptr->getRarity();
+    }
+    return false;
+}
+bool Item::operator!=(const Cargo& cargo) const {
+    if (typeid(cargo) == typeid(Item)) {
+        const Item* rarity_ptr = static_cast<const Item*>(&cargo);
+        return name_ != rarity_ptr->getName() ||
+               amount_ != rarity_ptr->getAmount() ||
+               basePrice_ != rarity_ptr->getBasePrice() ||
+               rairty_ != rarity_ptr->getRarity();
+    }
+    return false;
+}
---
+++ shm/Item.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+#include "Cargo.hpp"
+
+class Item : public Cargo {
+public:
+    enum class Rarity {
+        common = 1,
+        rare = 5,
+        epic = 10,
+        legendary = 20
+    };
+    Item(const std::string name, size_t amount, size_t basePrice, Rarity rarity)
+        : Cargo(name, amount, basePrice), rairty_(rarity) {}
+
+    size_t getPrice() const override;
+    std::string getName() const override;
+    size_t getAmount() const override;
+    size_t getBasePrice() const override;
+    Rarity getRarity() const;
+
+    bool operator==(const Cargo& item) const;
+    bool operator!=(const Cargo& item) const;
+    Cargo& operator+=(size_t amount);
+    Cargo& operator-=(size_t amount);
+
+private:
+    const Rarity rairty_;
+};
